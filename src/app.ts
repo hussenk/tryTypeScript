281f03042db3dc97f1f75007db840f28e369fc20
hussenk <hussen199710@yahoo.com>
2022-03-08 20:49:55 +0200

    + first route

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import config from 'config';
 import connect from './utils/connect';
 import logger from './utils/logger';
+import routes from './routes';
 
 const port = config.get<number>('port');
 
@@ -9,6 +10,10 @@ const app = express();
 
 
 app.listen(port, async () => {
+
     logger.info(`App running at http://127.0.0.1:${port}`);
+
     await connect();
-})
+
+    routes(app);
+});
