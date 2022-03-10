b9a1d312a13ccdf00617ce887e158c75bed5afe0
hussenk <hussen199710@yahoo.com>
2022-03-10 11:17:39 +0200

    + interface in model , extends mongoose.Document

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -3,6 +3,14 @@ import bcrypt from "bcrypt";
 import config from "config";
 
 
+export interface UserDocument extends mongoose.Document {
+    email: string;
+    name: string;
+    password: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
 const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     name: { type: String, required: true },
