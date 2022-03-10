940e744dc566dfdcf70254843a9c23ea7f96ef8d
hussenk <hussen199710@yahoo.com>
2022-03-10 23:01:50 +0200

    add schema middleware

---
+++ src/schema/user.schema.ts
@@ -0,0 +1,22 @@
+import { object, string } from "zod";
+
+export const createUserSchema = object({
+    body: object({
+        name: string(
+            { required_error: 'name is required', }
+        ),
+        password: string({
+            required_error: 'password is required'
+        }).min(6, 'Password to short - min 6 chars'),
+        passwordConfirmation: string({
+            required_error: 'passwordConfirmation is required',
+        }),
+        email: string({
+            required_error: 'email is required',
+        }).email('Not a valid email'),
+
+    }).refine((data) => data.password === data.passwordConfirmation, {
+        message: 'passwords do not match',
+        path: ["passwordConfirmation"],
+    }),
+});
