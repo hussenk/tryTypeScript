215c7646acdb839290da31397444e91f1f439738
hussenk <hussen199710@yahoo.com>
2022-03-10 23:07:22 +0200

    + use middleware in route

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,15 @@
 
 import { Express, Request, Response } from 'express';
 import { createUserHandler } from './controllers/user.controller';
+import validateResource from './middleware/validateResource';
+import { createUserSchema } from './schema/user.schema';
 
 function routes(app: Express) {
 
     app.get('/healthCheck', (req: Request, res: Response) => res.sendStatus(200));
 
 
-    app.post('/api/users', createUserHandler);
+    app.post('/api/users', validateResource(createUserSchema), createUserHandler);
 }
 
 export default routes;
